a8a41ab6192e2a2ad928d0200451ba09b690d63e
Diell02 <diellkepuska@gmail.com>
2021-12-26 18:14:55 +0100

    Diell02

--- Register.js
+++ Register.js
@@ -1,150 +1,195 @@
-function formValidation()
-{
-var uid = document.registration.userid;
-var passid = document.registration.passid;
-var uname = document.registration.username;
-var uadd = document.registration.address;
-var ucountry = document.registration.country;
-var uzip = document.registration.zip;
-var uemail = document.registration.email;
-var umsex = document.registration.msex;
-var ufsex = document.registration.fsex; if(userid_validation(uid,5,12))
-{
-if(passid_validation(passid,7,12))
-{
-if(allLetter(uname))
-{
-if(alphanumeric(uadd))
-{ 
-if(countryselect(ucountry))
-{
-if(allnumeric(uzip))
-{
-if(ValidateEmail(uemail))
-{
-if(validsex(umsex,ufsex))
-{
-}
-} 
-}
-} 
-}
-}
-}
-}
-return false;
+var username = document.forms['vform']['username'];
+var email = document.forms['vform']['email'];
+var password = document.forms['vform']['password'];
+var password_confirm = document.forms['vform']['password_confirm'];
+var address = document.forms['vform']['address'];
+var zipcode = document.forms['vform']['zipcode'];
+var phone = document.forms['vform']['phone'];
+var emri = document.forms['vform']['emri'];
+var lastname = document.forms['vform']['lastname'];
 
-} function userid_validation(uid,mx,my)
-{
-var uid_len = uid.value.length;
-if (uid_len == 0 || uid_len >= my || uid_len < mx)
-{
-alert("User Id should not be empty / length be between "+mx+" to "+my);
-uid.focus();
-return false;
-}
-return true;
-}
-function passid_validation(passid,mx,my)
-{
-var passid_len = passid.value.length;
-if (passid_len == 0 ||passid_len >= my || passid_len < mx)
-{
-alert("Password should not be empty / length be between "+mx+" to "+my);
-passid.focus();
-return false;
-}
-return true;
-}
-function allLetter(uname)
-{ 
-var letters = /^[A-Za-z]+$/;
-if(uname.value.match(letters))
-{
-return true;
-}
-else
-{
-alert('Username must have alphabet characters only');
-uname.focus();
-return false;
-}
-}
-function alphanumeric(uadd)
-{ 
-var letters = /^[0-9a-zA-Z]+$/;
-if(uadd.value.match(letters))
-{
-return true;
-}
-else
-{
-alert('User address must have alphanumeric characters only');
-uadd.focus();
-return false;
-}
-}
-function countryselect(ucountry)
-{
-if(ucountry.value == "Default")
-{
-alert('Select your country from the list');
-ucountry.focus();
-return false;
-}
-else
-{
-return true;
-}
-}
-function allnumeric(uzip)
-{ 
-var numbers = /^[0-9]+$/;
-if(uzip.value.match(numbers))
-{
-return true;
-}
-else
-{
-alert('ZIP code must have numeric characters only');
-uzip.focus();
-return false;
-}
-}
-function ValidateEmail(uemail)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-uemail.focus();
-return false;
-}
-} function validsex(umsex,ufsex)
-{
-x=0;
+var name_error = document.getElementById('name_error');
+var email_error = document.getElementById('email_error');
+var password_error = document.getElementById('password_error');
+var address_error = document.getElementById('address_error');
+var zipcode_error = document.getElementById('zipcode_error');
+var phone_error = document.getElementById('phone_error');
+var emri_error = document.getElementById('emri_error');
+var lastname_error = document.getElementById('lastname_error');
 
-if(umsex.checked) 
-{
-x++;
-} if(ufsex.checked)
-{
-x++; 
+username.addEventListener('blur', nameVerify, true);
+email.addEventListener('blur', emailVerify, true);
+password.addEventListener('blur', passwordVerify, true);
+address.addEventListener('blur', addressVerify, true);
+zipcode.addEventListener('blur', zipcodeVerify, true);
+phone.addEventListener('blur', phoneVerify, true);
+emri.addEventListener('blur', emriVerify, true);
+lastname.addEventListener('blur', lastnameVerify, true);
+
+function Validate() {
+ 
+  if (username.value == "") {
+    username.style.border = "1px solid red";
+    document.getElementById('username_div').style.color = "red";
+    name_error.textContent = "Username is required";
+    username.focus();
+    return false;
+  }
+
+  if (username.value.length < 3) {
+    username.style.border = "1px solid red";
+    document.getElementById('username_div').style.color = "red";
+    name_error.textContent = "Username must be at least 3 characters";
+    username.focus();
+    return false;
+  }
+
+  if (address.value == "") {
+    address.style.border = "1px solid red";
+    document.getElementById('address_div').style.color = "red";
+    address_error.textContent = "Address is required";
+    address.focus();
+    return false;
+  }
+  if (address.value.length < 3) {
+    address.style.border = "1px solid red";
+    document.getElementById('address_div').style.color = "red";
+    address_error.textContent = "Address must contain atleast more than 3 characters";
+    address.focus();
+    return false;
+  }
+
+  if (zipcode.value == "") {
+    zipcode.style.border = "1px solid red";
+    document.getElementById('zipcode_div').style.color = "red";
+    zipcode_error.textContent = "Zipcode is required";
+    zipcode.focus();
+    return false;
+  }
+  if (zipcode.value.length < 3) {
+    zipcode.style.border = "1px solid red";
+    document.getElementById('zipcode_div').style.color = "red";
+    zipcode_error.textContent = "Zipcode must contain atleast more than 3 numbers";
+    zipcode.focus();
+    return false;
+  }
+
+  if (phone.value == "") {
+    phone.style.border = "1px solid red";
+    document.getElementById('phone_div').style.color = "red";
+    phone_error.textContent = "Phone number is required";
+    phone.focus();
+    return false;
+  }
+  if (phone.value.length < 9) {
+    phone.style.border = "1px solid red";
+    document.getElementById('phone_div').style.color = "red";
+    phone_error.textContent = "Phone number must contain atleast more than 9 numbers";
+    phone.focus();
+    return false;
+  }
+
+  if (emri.value == "") {
+    emri.style.border = "1px solid red";
+    document.getElementById('emri_div').style.color = "red";
+    emri_error.textContent = "Name is required";
+    emri.focus();
+    return false;
+  }
+  if (emri.value.length < 2) {
+    emri.style.border = "1px solid red";
+    document.getElementById('emri_div').style.color = "red";
+    emri_error.textContent = "The name must contain more than 2 characters";
+    emri.focus();
+    return false;
+  }
+
+  if (lastname.value == "") {
+    lastname.style.border = "1px solid red";
+    document.getElementById('lastname_div').style.color = "red";
+    lastname_error.textContent = "Lastname is required";
+    lastname.focus();
+    return false;
+  }
+  if (lastname.value.length < 2) {
+    lastname.style.border = "1px solid red";
+    document.getElementById('lastname_div').style.color = "red";
+    lastname_error.textContent = "The lastname must contain more than 2 characters";
+    lastname.focus();
+    return false;
+  }
+
+  if (email.value == "") {
+    email.style.border = "1px solid red";
+    document.getElementById('email_div').style.color = "red";
+    email_error.textContent = "Email is required";
+    email.focus();
+    return false;
+  }
+
+  if (password.value == "") {
+    password.style.border = "1px solid red";
+    document.getElementById('password_div').style.color = "red";
+    password_confirm.style.border = "1px solid red";
+    password_error.textContent = "Password is required";
+    password.focus();
+    return false;
+  }
+
+  if (password.value != password_confirm.value) {
+    password.style.border = "1px solid red";
+    document.getElementById('pass_confirm_div').style.color = "red";
+    password_confirm.style.border = "1px solid red";
+    password_error.innerHTML = "The two passwords do not match";
+    return false;
+  }
 }
-if(x==0)
-{
-alert('Select Male/Female');
-umsex.focus();
-return false;
+
+function nameVerify() {
+  if (username.value != "") {
+   username.style.border = "1px solid #5e6e66";
+   document.getElementById('username_div').style.color = "#5e6e66";
+   name_error.innerHTML = "";
+   return true;
+  }
+
 }
-else
-{
-alert('Form Succesfully Submitted');
-window.location.reload()
-return true;
+
+function addressVerify(){
+  if (address.value != "") {
+    address.style.border = "1px solid #5e6e66";
+    document.getElementById('address_div').style.color = "#5e6e66";
+    address_error.innerHTML = "";
+    return true;
+   }
+ 
 }
+function emailVerify() {
+  if (email.value != "") {
+  	email.style.border = "1px solid #5e6e66";
+  	document.getElementById('email_div').style.color = "#5e6e66";
+  	email_error.innerHTML = "";
+  	return true;
+  }
 }
+function passwordVerify() {
+  if (password.value != "") {
+  	password.style.border = "1px solid #5e6e66";
+  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
+  	document.getElementById('password_div').style.color = "#5e6e66";
+  	password_error.innerHTML = "";
+  	return true;
+  }
+  if (password.value === password_confirm.value) {
+  	password.style.border = "1px solid #5e6e66";
+  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
+  	password_error.innerHTML = "";
+  	return true;
+  }
+}
+
+function login(){
+    
+  window.location = "Login.html";
+  
+  }
