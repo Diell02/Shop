75a817619d269d351754f2b662760103ab44652c
Diell02 <diellkepuska@gmail.com>
2022-02-25 20:11:57 +0100

    Diell

--- About.php
+++ About.php
@@ -22,13 +22,72 @@
             </div>
           </div>
 
-          <div class="paragraph">
-            <p>Kenzol lindi si ide nga Dion Nura dhe Diell Këpuska ne vitin 2021. Keta dy te rinj kishin si qellim shperndarjen e brendit te tyre te veshjeve si fillim ne nje treg te vogel sic eshte Kosova, dhe me pas te kapin treg te huaj. 
-              Qellimi i brendit tone te veshjeve eshte qe ti jap komfort dhe stilt e veqant te veshjes cdo konsumatori qe I perdor produktet tona, jane te gjitha kualitet i lart dhe prej pambuku. Dizajni i ketyre veshjeve eshte i vecant dhe kjo e bene mjaft atraktive Kenzol qe spikatet nga brendet e tjera te njohura ne rajon.
-            </p>
-          </div>
+            <div class="para">
+              <h2 id="titu">Our Story</h2>
+              <div id="linet"></div>
 
+            <p id="par1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet porttitor porttitor.
+              Mauris in purus turpis. Aliquam molestie lorem vel arcu accumsan, non porta tellus cursus. 
+              Nunc sed nulla a magna porttitor venenatis at sit amet sapien. Nullam gravida magna tincidunt
+              sodales varius. Praesent tincidunt, enim ac tempor feugiat, leo nulla convallis dui, 
+              id viverra libero urna sit amet lacus. Vivamus placerat, mi in fermentum tempor, nisi sem 
+              posuere mauris, id consequat est ipsum id elit. Ut iaculis, neque sit amet mollis viverra, 
+              felis justo gravida velit, ultricies gravida odio nisl ut quam. Sed at urna lectus. Duis 
+              pharetra efficitur ligula lacinia eleifend. Proin viverra massa elit, vel gravida mi 
+              vestibulum ut.</p>
+              
 
+            <p id="par2">Morbi dapibus tellus enim, eget luctus lacus dictum eget. Aliquam felis nibh, auctor id 
+              feugiat in, consequat ac augue. Vivamus vitae facilisis purus, in condimentum velit. 
+              Curabitur convallis sagittis magna, id accumsan turpis hendrerit quis. Donec venenatis 
+              facilisis nisi, a sollicitudin ante fermentum sit amet. Cras sit amet arcu ut lacus 
+              finibus tempus. Quisque placerat at elit vitae consectetur. Fusce in ex in tortor dapibus 
+              viverra vitae at urna. Curabitur in augue id est molestie dapibus. Praesent porta mattis
+              mi ut maximus. Curabitur a ipsum in lectus malesuada mattis. In in facilisis orci. 
+              Aenean in turpis dui. Nam dapibus erat at libero eleifend ullamcorper.</p>
+
+            <p id="par3">Praesent mi mauris, aliquam gravida metus et, commodo hendrerit augue. 
+              Sed molestie et ex nec finibus. Morbi quis ante quis nisi dignissim placerat. 
+              Sed tristique efficitur eros, a condimentum arcu ullamcorper eget. 
+              Pellentesque finibus orci ut eros vestibulum rutrum. Curabitur accumsan sodales viverra. 
+              Mauris venenatis turpis eu velit elementum, sit amet aliquet nulla aliquam. 
+              Nunc sed diam mauris. Ut maximus fermentum leo, ut ullamcorper nisl venenatis non. 
+              Curabitur lacinia tempor urna, at porttitor est interdum sit amet. 
+              Nullam tempor erat ac mi congue, vel tristique erat convallis.</p>
+
+            <p id="par4">Maecenas odio ipsum, tristique sed tellus vitae, pulvinar fermentum est. 
+              Morbi vehicula metus quis ex bibendum, rutrum fermentum eros ornare. 
+              Maecenas a ligula placerat, rutrum erat vitae, bibendum dolor. 
+              Integer vestibulum auctor mi, sit amet laoreet nisl luctus quis. 
+              Ut id lacus mollis, aliquet diam quis, varius risus. 
+              Donec pulvinar purus commodo tincidunt faucibus. Pellentesque condimentum, 
+              justo auctor eleifend rhoncus, velit velit aliquet ligula, et sodales quam sapien nec enim.
+              Curabitur tortor magna, feugiat eget placerat quis, varius eget turpis. 
+              Aenean iaculis quis turpis sit amet placerat.</p>
+
+            <p id="par5">Suspendisse malesuada sem quis fermentum ultrices. 
+              Praesent sollicitudin ante in arcu accumsan, quis gravida risus eleifend. 
+              Nulla facilisi. Etiam sit amet posuere turpis. Morbi dictum mi tortor, 
+              sit amet elementum ipsum mattis non. Nunc ligula eros, rutrum nec fringilla vulputate, 
+              lacinia vel nunc. Suspendisse potenti. Proin consectetur id ante et eleifend. 
+              Pellentesque eros nisl, varius et nunc non, facilisis vestibulum enim. Nunc vel nibh posuere,
+              sollicitudin dolor nec, sollicitudin tortor. Nam pretium nisi ut urna consequat, 
+              at scelerisque massa sagittis.</p>
+              
+              </div>
+              <h2 id="titullf">Founders</h2>
+              <div class="founders">
+                
+              <div id="pers1">
+              <div class="img1"><img src="Pics/Black.png" width="180px", height="180px"></div>
+              <h3 id="texti1">Diell</h3></div>
+              
+              <div id="pers2">
+              <div class="img2"><img src="Pics/Black.png" width="180px", height="180px"></div>
+              <h3 id="texti2">Dion</h3></div>
+              </div>
+
+            <div class="line"></div>
           <div class="footer">
             <div class="footer-left">
               <a href="Home.php" class="logo"><img src="Pics/Black.png" width="40px", height="40px">Kenzol</a>
--- Shop.php
+++ Shop.php
@@ -78,17 +78,37 @@
             <br><a href="Shop.php"><button id="buy-R">Buy</button></a></div>
         </div>
 
+        <div class="Shirt-L">
+          <img src="Pics/AquaGreen.png" width="250px", height="250px">
+          <div class="Shirts-L">
+            <h1 id="emri">Bluze</h1>
+            <h2 id="cmimi">15.99$</h2>
+            <p id="par">Perdorimi: Vrapim<br>Materiali: Pambuk 90%, Poliester 10%</p>
+            <br><a href="Shop.php"><button id="buy-L">Buy</button></a></div>
+        </div>
 
-          <div class="footer">
-            <div class="footer-left">
-              <a href="Home.php" class="logo"><img src="Pics/Black.png" width="40px", height="40px">Kenzol</a>
-              <ul id="ul1">
-                <li><a href="Home.php">Home</a></li>
-                <li><a href="Contact.php">Contact</a></li>
-                <li><a href="About.php">About</a></li>
-                <li><a href="Login.php">Login</a></li>
-              </div>
-              <div class="footer-right">
+        <div class="Shirt-R">
+          <img src="Pics/DarkRed.png" width="250px", height="250px">
+          <div class="Shirts-R">
+            <h1 id="emri">Bluze</h1>
+            <h2 id="cmimi">15.99$</h2>
+            <p id="par">Perdorimi: Vrapim<br>Materiali: Pambuk 90%, Poliester 10%</p>
+            <br><a href="Shop.php"><button id="buy-R">Buy</button></a></div>
+        </div>
+
+          <div class="line"></div>
+      <div class="visline"></div>
+      
+    <div class="footer">
+      <div class="footer-left">
+        <a href="Home.php" class="logo"><img src="Pics/Black.png" width="40px", height="40px">Kenzol</a>
+          <ul id="ul1">
+            <li><a href="Home.php">Home</a></li>
+            <li><a href="Contact.php">Contact</a></li>
+            <li><a href="About.php">About</a></li>
+            <li><a href="Login.php">Login</a></li></ul>
+            </div>
+          <div class="footer-right">
                 <a href="https://www.facebook.com" class="icon"><img src="Pics/fb.png" width="30px", height="30px"></a>
                 <a href="https://www.twitter.com" class="icon"><img src="Pics/tw.png" width="30px", height="30px"></a>
                 <a href="https://www.google.com" class="icon"><img src="Pics/g.png" width="30px", height="30px"></a>
@@ -97,8 +117,8 @@
                 <ul id="ul2"><li>Phone:+383123123</li>
                   <li>Email:ubt-uni@ubt-uni.net</li>
                   <li>Adresa:Nena Tereze</li></ul></div> 
-          </div>
-    </body>
+    </div>
+  </body>
     
 </html>
 
